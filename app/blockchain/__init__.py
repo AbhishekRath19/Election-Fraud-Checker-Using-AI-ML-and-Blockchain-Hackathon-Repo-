75aabef478b469997b2120798169761a9988280e
kaashviigoel <kaashvigoel1508@gmail.com>
2025-08-25 14:17:54 +0530

    Delete app directory

--- app/blockchain/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-blockchain_bp = Blueprint('blockchain', __name__)
-
-from . import web3_integration, sync, sync_routes
--- app/voting/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-voting_bp = Blueprint('voting', __name__)
-
-from . import vote_processing
